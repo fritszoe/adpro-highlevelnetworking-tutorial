70e084d7ef6cdc31960b30c21a89844727914cb6
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-05-01 15:56:28 +0700

    Transaction Service Implementation

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         .compile(
             &["proto/services.proto"],
-            &["proto"], 
+            &["proto"],
         )?;
     Ok(())
 }
